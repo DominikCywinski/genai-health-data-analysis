558ab612c779e4ad9b9be9edde8cb7999bc1e48a
DominikCywinski <dominik.cywinski988@gmail.com>
2024-11-17 19:12:59 +0100

    Add prompts and SQLResponseGenerator class for generating SQL queries and natural language responses.

---
+++ src/model.py
@@ -0,0 +1,52 @@
+import os
+import google.generativeai as genai
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from src.prompts import get_prompts
+from src.sql_database import execute_sql_query
+
+
+class SQLResponseGenerator:
+    def __init__(self, api_key=None):
+        load_dotenv()
+        if api_key is None:
+            api_key = os.getenv("GOOGLE_API_KEY")
+        genai.configure(api_key=api_key)
+        sql_prompt, response_prompt = get_prompts()
+
+        self.sql_prompt = sql_prompt
+        self.response_prompt = response_prompt
+        self.llm = genai.GenerativeModel("gemini-pro")
+
+        self.sql_template = PromptTemplate(
+            input_variables=["question"], template=sql_prompt
+        )
+        self.response_template = PromptTemplate(
+            input_variables=["results", "question"], template=response_prompt
+        )
+
+    def generate_sql_query(self, question):
+        prompt = self.sql_template.format(question=question)
+        response = self.llm.generate_content([prompt, question])
+
+        return response.text
+
+    def generate_natural_language_response(self, results, question):
+        print(results)
+        print(type(results))
+        prompt = self.response_template.format(results=results, question=question)
+        print(prompt)
+        response = self.llm.generate_content([prompt, question])
+        return response.text
+
+
+# For TESTING purpose
+# model = SQLResponseGenerator()
+# question = "how many people with blood presure have bmi larger than 45?"
+# sql_query = model.generate_sql_query(question)
+# print(f"Generated SQL Query: {sql_query}")
+# sql_query = "SELECT AVG(t1.salt_content_in_the_diet) AS Average_Salt_Content FROM dataset1 AS t1 JOIN dataset2 AS t2 ON t1.Patient_Number = t2.Patient_Number WHERE t2.Average_Steps_10_days > 10000;"
+# sql_results = execute_sql_query(sql_query)
+# print(sql_results)
+# response = model.generate_natural_language_response(sql_results, question)
+# print(response)
---
+++ src/prompts.py
@@ -0,0 +1,74 @@
+from src.sql_database import get_dataset_columns_dict
+from src.utils import get_datasets_list
+
+
+def get_prompts():
+    datasets_list = get_datasets_list()
+    dataset_columns_dict = get_dataset_columns_dict(datasets_list)
+    common_column = dataset_columns_dict["dataset1"][0]
+
+    column_names_str = "\n".join(
+        [f"{key}={', '.join(value)}" for key, value in dataset_columns_dict.items()]
+    )
+
+    sql_prompt = f"""
+        You are an expert in SQL query generation. 
+        You have access to a database named 'database.db' 
+        with the following datasets: {', '.join(map(str, datasets_list))}.
+        The common column between these datasets is {common_column}
+        You have specified column names for each dataset respectively: 
+        {column_names_str}
+        
+        Instructions:
+        1. The user will provide a specific question and require answer based on the datasets.
+        2. If you REALLY need information from multiple datasets, use the {common_column} column to join the datasets.
+        3. Generate a valid SQL query to answer the user's question. 
+        
+        Use the following assumptions where applicable:
+        - `Blood_Pressure_Abnormality`: 0 -> normal, 1 -> abnormal.
+        - `Sex`: 0 -> male, 1 -> female.
+        - `Pregnancy`: 0 -> not pregnant, 1 -> pregnant.
+        - `Smoking`: 0 -> does not smoke, 1 -> smokes.
+        - `Chronic_kidney_disease` and `Adrenal_and_thyroid_disorders`: 0 -> no disease, 1 -> has the condition.
+        - `salt_content_in_the_diet` and `alcohol_consumption_per_day` are numerical values representing daily intake
+           in milligrams and milliliters, respectively.
+        - `Level_of_Stress`: Numerical scale where higher values indicate greater stress levels.
+        - 'Average_Steps(10 days)': average number of steps per day in the last 10 days
+        - 'Activity_Level': sedentary, lightly_active, moderately_active, very_active
+        
+        EXAMPLES:
+        
+        Example 1 - "What are the average BMI values?",
+        the SQL command will be something like this:
+        SELECT AVG(BMI) FROM dataset1;
+        
+        Example 2 - "What is the average BMI for patients, who average make more than 10000 steps per day?",
+        the SQL command will be something like this:
+        SELECT AVG(t1.BMI) AS Average_BMI 
+        FROM dataset1 AS t1 
+        JOIN dataset2 AS t2 ON t1.Patient_Number = t2.Patient_Number 
+        GROUP BY t1.Patient_Number 
+        HAVING AVG(t2.Physical_activity) >= 10000;
+        
+        Example 3 - "What is the average salt content in the diet patients, who average make more than 10000 steps per day?",
+        the SQL command will be something like this:
+        SELECT AVG(t1.salt_content_in_the_diet) AS Average_Salt_Content 
+        FROM dataset1 AS t1 
+        JOIN dataset2 AS t2 ON t1.Patient_Number = t2.Patient_Number 
+        WHERE t2.Average_Steps_10_days > 10000;
+        
+        also the sql code should not have ``` in beginning or end and sql word in output
+        """
+
+    response_prompt = """
+        You are an expert in natural language processing and SQL response interpretation.
+        You have provided:
+        - User's input question: "{question}"
+        - SQL query results based on the input question: {results}
+        
+        return the answer to the User's input question in natural language based on question and query results.
+        Sometimes you might need to calculate some value based on the query results.
+        Use full sentences and avoid assumptions or external knowledge not present in the query results.
+        """
+
+    return sql_prompt, response_prompt
